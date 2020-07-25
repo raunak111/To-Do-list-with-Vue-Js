8313cd0128ca6912d65259d3d1207a89c7d57b3d
raunak111 <justlikethat424@gmail.com>
2020-07-26 02:26:04 +0530

    completed functionality of the todo app. still needs better ui though

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
   </div>
 </template>
-
 <script>
-
-
+import GetTodo from './components/GetTodo.vue'
+import CurrentTodos from './components/CurrentTodos.vue'
+import CompletedTodos from './components/CompletedTodos.vue'
 export default {
+  components: {
+    GetTodo,
+    CurrentTodos,
+    CompletedTodos
+  }
  
 }
 </script>
